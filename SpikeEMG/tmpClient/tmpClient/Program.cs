b6a298ccd2f9b1bde04952423f86d8748e45ab53
MIngjianZhang <mzhang41@u.rochester.edu>
2018-03-13 11:40:45 -0400

    Spike EMG data
    The old EMG machine old we have used

---
+++ SpikeEMG/tmpClient/tmpClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+public class UWPClient
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            int port = 1337;
+            IPEndPoint ip = new IPEndPoint(IPAddress.Parse("192.168.43.221"), port);
+            TcpClient client = new TcpClient();
+            //client.NoDelay = true;
+            //if (client.NoDelay == true)
+            //    Console.WriteLine("Nodelay set to true ");
+            Console.WriteLine("Connecting...\n");
+            client.Connect(ip);
+            Console.WriteLine("Connected!\n");
+            using (NetworkStream ns = client.GetStream())
+            {
+                using (StreamReader sr = new StreamReader(ns))
+                {
+                    StreamWriter sw = new StreamWriter(ns);
+                    sw.WriteLine((10.0)*Double.Parse(args[0])+(-10.0)*Double.Parse(args[1]));
+                    sw.Flush();
+                    DateTime localDate = DateTime.Now;
+                    Console.WriteLine("Time is ~~~ " + localDate);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+}
