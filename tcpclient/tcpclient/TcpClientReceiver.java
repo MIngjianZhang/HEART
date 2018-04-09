6e3d3cf3638c4d8f12ce91ff6482845e0b2ce22d
MIngjianZhang <mzhang41@u.rochester.edu>
2018-04-09 19:38:59 -0400

    some changes in Delsys

--- tcpclient/tcpclient/TcpClientReceiver.java
+++ tcpclient/tcpclient/TcpClientReceiver.java
@@ -12,6 +12,10 @@ import java.nio.charset.StandardCharsets;
          String temp;
          float displayFloat;
          int counter = 0;
+         int test =0; //Just for debugging purposes
+         float temp1 = 0;
+         float temp2;
+         boolean send = true;
          boolean valid = true;
         try
         {
@@ -40,19 +44,33 @@ import java.nio.charset.StandardCharsets;
         {
 	       displayFloat = inFromServer.readFloat();
          counter++;
+         //test++;
          if(valid){
           if(counter % 16 == 0){
             valid = false;
             counter = 0;
+            
           }
          }else{
           if(counter % 16000 == 0){
             valid = true;
             counter = 0;
+            
           }
          }
          if(valid){
-          System.out.println(displayFloat);
+            if (counter == 1 || counter == 2){
+            System.out.println(displayFloat);
+            //System.out.println(test);
+            //This will print stuff like 1,2,160016,160017
+            send = !send;
+            if (send){
+              temp2 = displayFloat;
+              System.out.println("The first number is "+temp1 +" and the second one is " + temp2);
+              //Process process = new ProcessBuilder("C:\\Users\\ROC-HCI-1\\Downloads\\HEART-20170925T165908Z-001\\HEART\\tmpClient\\tmpClient\\bin\\Debug\\tmpClient.exe","temp1","temp2").start();
+            }
+            temp1 = displayFloat;
+          }
          }
          
         }
