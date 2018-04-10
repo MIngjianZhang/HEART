f048bd66013dac2b25d638366c9ace6296bb7f94
MIngjianZhang <mzhang41@u.rochester.edu>
2018-04-10 19:41:12 -0400

    update

--- tcpclient/tcpclient/TcpClientReceiver.java
+++ tcpclient/tcpclient/TcpClientReceiver.java
@@ -1,5 +1,6 @@
   import java.io.BufferedReader;
   import java.io.InputStreamReader;
+  import java.io.IOException;
   import java.net.Socket;
   import java.io.*;
   import java.nio.charset.StandardCharsets;
@@ -19,6 +20,12 @@
          float temp2;
          boolean send = true;
          boolean valid = true;
+         File dir = new File ("C://Users//ROC-HCI-1//Downloads//HEART-20170925T165908Z-001//HEART//tmpClient//tmpClient//bin//Debug");
+         String[] argument = new String[2];
+         //argument[0] = "notepad.exe";
+         argument[0] = "haha";
+         Runtime runtime = Runtime.getRuntime();
+
         try
         {
         //create input stream
@@ -31,14 +38,11 @@
         DataInputStream inFromServer = new DataInputStream(clientSocket.getInputStream());
 		
 		    ByteBuffer dataBuf = ByteBuffer.allocate(12);
-
-
         //send line to server
         //outToServer.writeBytes(temp);
 
        //read line from server
         //displayBytes = inFromServer.readLine();
-
         while(true)
         {
 	       byte[] tempBytes = new byte[4];
@@ -59,7 +63,7 @@
           }
          }else{
           if(counter % 15984 == 0){
-		//manually set the frequency to 2HZ...
+        		//manually set the frequency to 2HZ...
             valid = true;
             counter = 0;
             
@@ -73,10 +77,18 @@
             send = !send;
             if (send){
               temp2 = displayFloat;
+              argument[1] = Float.toString(temp2);
               System.out.println("The first number is "+temp1 +" and the second one is " + temp2);
-              Process process = new ProcessBuilder("C:\\Users\\ROC-HCI-1\\Downloads\\HEART-20170925T165908Z-001\\HEART\\tmpClient\\tmpClient\\bin\\Debug\\tmpClient.exe","temp1","temp2").start();
+              System.out.println(argument[0] + " hhhhh "+ argument[1]);
+              Process process = new ProcessBuilder("C:\\Users\\ROC-HCI-1\\Downloads\\HEART-20170925T165908Z-001\\HEART\\tmpClient\\tmpClient\\bin\\Debug\\tmpClient.exe","1.1","1.0").start();
+              /*try{
+              Process p = runtime.exec(new String[]{"tmpClient.exe","0.5","1.0"}, null , dir);
+              }catch(IOException e){
+                e.printStackTrace();
+              }*/
             }
             temp1 = displayFloat;
+            argument[0] = Float.toString(temp1);
           }
          }
          
